7e95985ca1abb7f0d2efe664fb0bf0f3e5179e8b
Zachary Hutto <zacharyhutto@Zacharys-MacBook-Air.local>
2018-06-09 14:05:05 -0600

    almost finished login page

--- web/src/ui/pages/unsecured/login/index.js
+++ web/src/ui/pages/unsecured/login/index.js
@@ -29,27 +29,37 @@ class Login extends Component {
   }
 
   render() {
-    // Create a PageTemplate element
-    //  //create a div with a class of title
-    //    //Add the title you want
-    //  //create a div with a class of description
-    //    //Add your description
-    //  //Create a form with a class of form
+    // Create a PageTemplate element-
+    //  //create a div with a class of title-
+    //    //Add the title you want-
+    //  //create a div with a class of description-
+    //    //Add your description-
+    //  //Create a form with a class of form-
+    //    //Create an input with:-
+    //      //a placeholer attribute of "Your email"-
+    //      //a type attribute of text-
+    //      //a value attribute of this.state.email (note this is javascript)-
+    //      //a onChange attribute of this.handleInputEmail (note this is javascript)-
     //    //Create an input with:
-    //      //a placeholer attribute of "Your email"
-    //      //a type attribute of text
-    //      //a value attribute of this.state.email (note this is javascript)
-    //      //a onChange attribute of this.handleInputEmail (note this is javascript)
-    //    //Create an input with:
-    //      //a placeholer attribute of "Enter Password"
-    //      //a type attribute of password
-    //      //a value attribute of this.state.password (note this is javascript)
-    //      //a onChange attribute of this.handleInputPassword (note this is javascript)
-    //    //Create a react button:
-    //      //that handles a click calling this.handleClick
-    //      //make sure to set its type to a button
+    //      //a placeholer attribute of "Enter Password"-
+    //      //a type attribute of password-
+    //      //a value attribute of this.state.password (note this is javascript)-
+    //      //a onChange attribute of this.handleInputPassword (note this is javascript)-
+    //    //Create a react button:-
+    //      //that handles a click calling this.handleClick-
+    //      //make sure to set its type to a button-
     return (
-      <span>TODO delete me</span>
+      <PageTemplate>
+        <div styleName="title">Zack's Page</div>
+        <div styleName="description">Enter your email and password</div>
+        <form styleName="form">
+          <input placeholder="Your email" attribute="text" value={this.state.email}
+            onChange={this.handleInputEmail} />
+          <input placeholder="Enter Password" attribute="password" value={this.state.password}onChange={this.handleInputPassword} />
+        </form>
+        <button type="button" onClick={this.handleClick}>Login</button>
+        <a styleName="forgot" href="#">Forgot Password?</a>
+      </PageTemplate>
     )
   }
 }
