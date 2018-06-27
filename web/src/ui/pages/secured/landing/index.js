9a811d7cac4be0a17311cabac22f2f061bd669da
Bryce Hayden <bryce-hayden@pluralsight.com>
2018-06-26 21:05:13 -0600

    Example in class about siblings props/state

--- web/src/ui/pages/secured/landing/index.js
+++ web/src/ui/pages/secured/landing/index.js
@@ -2,10 +2,23 @@ import React, { Component } from 'react'
 import { Link, withRouter } from 'react-router-dom'
 import CSSModules from 'react-css-modules'
 
+import Mood from './mood'
+import MoodFilter from './mood-filter'
 import css from './index.css'
 import connected from 'State/connect'
 
 class LandingPage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      parentValue: ''
+    }
+  }
+
+  handleZipChange = (event) => {
+    event.preventDefault()
+    this.setState({ parentValue: event.target.value })
+  }
   render() {
     // Create a div with a class of container
     //  //Create a header with the title of Challenges
@@ -21,6 +34,8 @@ class LandingPage extends Component {
             <Link to="/challenges/pyramid">Pyramid Challenge</Link>
           </li>
         </ol>
+        <Mood zipCode={this.state.parentValue} zipChange={this.handleZipChange} />
+        <MoodFilter zipCode={this.state.parentValue} zipChange2={this.handleZipChange} />
       </div>
     )
   }
--- web/src/ui/pages/secured/landing/mood-filter.js
+++ web/src/ui/pages/secured/landing/mood-filter.js
@@ -3,21 +3,24 @@ import React, { Component } from 'react'
 
 const { string, func } = PropTypes
 
-class MoodComp extends Component {
+class MoodFilterComp extends Component {
   static propTypes = {
     zipCode: string.isRequired,
-    zipChange: func.isRequired
+    zipChange2: func.isRequired
   }
+
   render() {
     return (
       <div>
-        <input type="text"
+        I'm the mood filter
+        <input
+          type="text"
           value={this.props.zipCode}
           placeholder="Enter a zip code"
-          onChange={this.props.zipChange} />
+          onChange={this.props.zipChange2} />
       </div>
     )
   }
 }
 
-export default MoodComp
+export default MoodFilterComp
